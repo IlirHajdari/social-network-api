52683e6536e77b62f766fe9f969cb16d72964b49
Ilir <ilir.hajdari111@gmail.com>
2023-08-05 02:43:14 -0400

    changed the file names of some files and changed post to thought since that is the assignment.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const postRoutes = require("./postRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 
-router.use("/posts", postRoutes);
+router.use("/thoughts", thoughtRoutes);
 router.use("/users", userRoutes);
 
 module.exports = router;
